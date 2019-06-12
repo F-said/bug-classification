29f5183442efc2e5fca866a6db0d0f91fbe0d8f0
saidmf <saidmf@rpi.edu>
2019-06-12 16:56:37 -0400

    LoadDataFiles gets and copies buggy files

--- LoadDataFiles.py
+++ LoadDataFiles.py
@@ -1,29 +1,29 @@
 """
 Script to create java files that contain buggy code. Could be done in console, but writing in file for documentation
-purposes
 """
 
 filetype = "java"
 
+# Path on local machine to data files. Change to your own path if running on local machine
+path = "/Users/farukhsaidmuratov/Desktop/SemesterFolder/Summer2019/NSFREUNJIT/Code-and-Data/Pig/"
+
 # Load PigBuggyLines
 buggyfiles = open("PigBuggyLines.txt", "r")
-buggyfile_list = []
+path_list = []
 
-# Extract file names of buggy lines
+# Extract paths of buggy lines
 for line in buggyfiles:
     # If line contains file name
     if line[-5:-1] == filetype:
-        # Find last instance of forward slash to get file name. Add 1 to remove forward slash
-        begin_ind = line.rfind("/") + 1
-        file_name = line[begin_ind:-1]
+        # If path not already recorded then record
+        if line not in path_list:
+            path_list.append(line[:-1])
 
-        # If file name not already recorded then record
-        if file_name not in buggyfile_list:
-            buggyfile_list.append(file_name)
+# Write file names of buggy lines and copy file at path to the empty file name
+for file in path_list:
+    # Find last instance of forward slash to get file name. Add 1 to remove forward slash
+    begin_ind = file.rfind("/") + 1
+    file_name = file[begin_ind:]
 
-# Write empty file names of buggy lines
-for file in buggyfile_list:
-    writefile = open(file, "w")
-    writefile.close()
-
-# Copy path to file that matches the name
+    # create file of "file_name" and copy code from file that is in the path
+    open(file_name, "w").writelines([l for l in open(path + file, "r").readlines()])
