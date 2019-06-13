466bf2abb5c125454bfd1c2a62da72358eee994c
Peter Boyland <pboyland@andrew.cmu.edu>
2019-06-13 15:06:33 -0400

    made it merge multi-line statements together

--- applyw2v.py
+++ applyw2v.py
@@ -11,15 +11,32 @@ def filterNonsense(sent):
 		return False
 	if sent[0] == "*":
 		return False
+	if sent[0] == "\\" and sent[1] == "\\":
+		return False
+	if sent[0] == "(" and sent[1] == "*":
+		return False
 	return True
 
+def joinstatement(sents):
+	curwordlist = []
+	res = []
+	for sent in sents:
+		curwordlist.append(sent)
+		if sent[-1] == ";" or sent[-1] == "{" or sent[-1] == "}":
+			res.extend(curwordlist)
+			curwordlist = []
+	return res
+
 def main():
-	w2v_model = Word2Vec(min_count=20,window=2,size=400,sample=6e-5,alpha=0.03,min_alpha=0.0007,negative=20)
+	w2v_model = Word2Vec(min_count=20,window=2,size=300,sample=6e-5,alpha=0.03,min_alpha=0.0007,negative=20)
 	with open("PigCode.java",'r') as f:
 		lines = f.readlines()
 	sents = [tokenizeCode(line) for line in lines]
 	filtered_sentences = [sent for sent in sents if filterNonsense(sent)]
-	#print(filtered_sentences[420])
+	joined_sentences = joinstatement(filtered_sentences)
+
+	#NOTE: intuitive performance appears to be better when the raw sentences are used without filtering or joining
+	sents = joined_sentences
 	w2v_model.build_vocab(sents,progress_per=1000)
 	w2v_model.train(sents, total_examples=w2v_model.corpus_count, epochs=30,report_delay=1)
 	w2v_model.init_sims(replace=True)
@@ -30,7 +47,7 @@ def print_exploration(w2v_model):
 	print(w2v_model.wv.similarity("!", "="))
 	print(w2v_model.wv.doesnt_match(['private', 'protected', 'if']))
 	#what is to - as * is to +?
-	print(w2v_model.wv.most_similar(positive=["-", "*"], negative=["+"], topn=3))
+	print(w2v_model.wv.most_similar(positive=["-", "*"], negative=["+"], topn=7))
 
 if __name__ == '__main__':
 	main()
