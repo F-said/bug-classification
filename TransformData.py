333fea3d263f16459f90ce0283394e6b2738947f
saidmf <saidmf@rpi.edu>
2019-06-14 11:27:00 -0400

    Started file to transform statements into matrix

---
+++ TransformData.py
@@ -0,0 +1,8 @@
+from applyw2v import tokenizeCode, filterNonsense, joinstatement
+from gensim.models import Word2Vec
+
+"""
+Script to transform all statements into matrix of word vectors, with padding
+"""
+
+### COOKING BEANS AND THEN DOING THIS BRB ###
