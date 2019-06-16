630d142c92d830586313ef2e3a22587e647b1b3e
Peter Boyland <pboyland@andrew.cmu.edu>
2019-06-15 23:43:10 -0400

    padding

--- TransformData.py
+++ TransformData.py
@@ -3,11 +3,16 @@ import gensim.models
 import csv
 import ast
 import numpy as np
+import tensorflow as tf
+#import Simple_rnn_and_simple_cnn as sm
 """
 Script to transform all statements into matrix of word vectors, with padding
 """
 
-def main():
+def pad(beglist, padl, pade):
+	return (beglist + padl * [pade])[:padl]
+
+def main(debug=False):
 	w2v_model = gensim.models.Word2Vec.load('w2v_model.bin')
 	with open('bug-classification.csv') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
@@ -20,15 +25,17 @@ def main():
 	maxtokens = max([len(stm) for stm in buggy_lines])
 	fine_lines = [line for line in fine_lines if len(line) <= maxtokens]
 
-	buggy_vecs = [[w2v_model[word] for word in line] for line in buggy_lines]
-	fine_vecs = [[w2v_model[word] for word in line] for line in fine_lines]
+	worddim = len(w2v_model[buggy_lines[0][0]])
+	buggy_vecs = [pad([w2v_model[word] for word in line],maxtokens,np.array(worddim*[0.0])) for line in buggy_lines]
+	fine_vecs = [pad([w2v_model[word] for word in line],maxtokens,np.array(worddim*[0.0])) for line in fine_lines]
+	
 
-	print(str(len(buggy_lines)) + " buggy lines")
-	print(str(len(fine_lines)) + " lines without bugs")
-	print(buggy_lines[3])
-	print(buggy_vecs[3])
-
+	if debug:
+		print(str(len(buggy_lines)) + " buggy lines")
+		print(str(len(fine_lines)) + " lines without bugs")
+		print(str(maxtokens) + " tokens per line")
+		print(str(worddim) + " dimension of token")
+		#print(buggy_vecs[42])
+		
 if __name__ == '__main__':
-	main()
-
-### COOKING BEANS AND THEN DOING THIS BRB ###
+	main(True)
